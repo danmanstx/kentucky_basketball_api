f36604c24b98d643ac03773a85d18f178ddcd4ec
DrSayre <DrSayre2002@yahoo.com>
2015-04-17 12:37:47 -0400

    init

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,4 @@
 class Game < ActiveRecord::Base
-  belongs_to :home_team
-  belongs_to :away_team
+  belongs_to :home_team, class_name: 'Team', foreign_key: 'home_team_id'
+  belongs_to :away_team, class_name: 'Team', foreign_key: 'away_team_id'
 end
